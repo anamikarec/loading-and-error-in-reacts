ecfbc84c836ea765e3222edda8386d981d93aaad
Anamika Gupta <65801101+anamikarec@users.noreply.github.com>
2021-12-01 09:21:36 +0530

    behaviour of useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import "./styles.css";
-
+import { useEffect } from "react";
 export default function App() {
+  console.log(1);
+  useEffect(() => {
+    console.log("useffect");
+  });
+  console.log(2);
   return (
     <div className="App">
       <h1>Loading and Error Concept</h1>
